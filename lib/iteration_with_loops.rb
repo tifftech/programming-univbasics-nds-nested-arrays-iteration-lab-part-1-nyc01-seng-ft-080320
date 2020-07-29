59917cc72160f75f8697b8debe5719ee40b06feb
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-29 21:29:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,13 @@
 def find_even_values(src)
-  array_of_arrays = [
-    [10,11],
-    [99,50,3,4]
-    [23,41]
-]
-
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].even?
+        p src[row_index][element_index]
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
 end
